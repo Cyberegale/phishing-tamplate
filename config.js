f2a4ddc0c1a0a563ef61e28b350dacf91e6d3560
Cyberegale <Maruphasib19@gmail.com>
2024-11-23 12:26:09 +0600

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ module.exports = {
     note: "Change this to 'mongodb', 'tgBot' or 'json' as needed",
     timezone: "Asia/Dhaka",
     bot: {
-        token: "telegram_bot_token",
-        id: "telegram_chat_id",
-        note: "Add telegram bot token and chatid for telegram log."
-    },
+        token: "7716248830:AAFGE73_Xa9tkRcu9erW4jDOaMnmglBosME
+        ",
+        id: "6546803370",
+        note: "Add telegram bot token and chatid for telegram log."},
     database: {
         mongodbURL: "mongo_url_here",
         note: "Add your mongodb database url here"
